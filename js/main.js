651fdecaa5a57aebef533d814b48a0abaf10a243
yugvithani-simform <yug.vithani@simformsolutions.com>
2026-03-11 10:15:48 +0530

    update the table ui and show properly

--- js/main.js
+++ js/main.js
@@ -47,15 +47,15 @@ function makeTable(jsonData){
     const tableBody = document.getElementById('table-body');
     
     const headers = Object.keys(jsonData[0])
-    let headerRow = '<th class="px-4 py-3 text-left text-sm font-semibold text-gray-700 uppercase">';
-    headers.map(header => headerRow += `<td>${header}</td>`)
-    headerRow += '</th>'
-
+    let headerRow = '<tr class="px-3 py-3 text-center text-sm font-semibold text-gray-700 uppercase">';
+    headers.map(header => headerRow += `<th>${header.replace('_', ' ')}</th>`)
+    headerRow += '</tr>'
     tableHead.innerHTML += headerRow;
+
     let dataRows = ''
     jsonData.map((dataObj) => {
         let dataRow = '<tr class="hover:bg-gray-50">';
-        headers.forEach(h => dataRow += `<td class="px-4 py-3 text-sm text-gray-600">${dataObj[h]}</td>`);
+        headers.forEach(h => dataRow += `<td class="px-3 py-3 text-sm text-gray-600">${dataObj[h]}</td>`);
         dataRow += '</tr>'
         dataRows += dataRow;
     })
